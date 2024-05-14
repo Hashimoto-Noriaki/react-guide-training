5bd3e2894885256bd8e82f9e87e66d3d4a824f19
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-15 00:14:32 +0900

    リファクタリング

--- 05_state_and_event/src/070_state_and_component/start/Example.jsx
+++ 05_state_and_event/src/070_state_and_component/start/Example.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
-const Practice = () => {
+const Example = () => {
   const [toggle,setToggle] = useState(true);
   const toggleComponent = () => {
     setToggle(prev => !prev);//toggleのボタンを押すとカウントAとBの入れ替え
   }
   return(
     <>
+    {/* コンポーネントの位置によってstateが識別される */}
     <button onClick={toggleComponent}>toggle</button>
     {toggle ? <Count title="カウントA"/>:<Count title="カウントB"/>} {/*三項演算子*/} 
        {/* <Count title="カウントA"/>{toggle && <Count title="B"/>} */}
@@ -15,7 +16,7 @@ const Practice = () => {
   )
 }
 
-const Count = () => {
+const Count = ( {title} ) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -25,7 +26,7 @@ const Count = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>カウント: {title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
