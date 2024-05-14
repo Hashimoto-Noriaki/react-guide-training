ae3f5194d6922bcc86fa40a1414de5dc1370dece
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-05-14 23:07:59 +0900

    オブジェクトのステート

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
 
 const Example = () => {
-  const personObj = { name: "Tom", age: 18 };
+  const personObj = { name: "大谷", age: 29 };//初期値
+  const [person,setPerson] = useState(personObj);//useState
+
+  const changeName = (e) => {
+    setPerson({name:e.target.value,number:person.age});
+  }
+
+  const changeAge = (e) => {
+    setPerson({name:person.name,age:e.target.value})
+  }
+
+  const reset = () => {
+    setPerson({name:"",age:""})//空
+  }
+
+  return (<>
+    <h3>名前:{person.name}</h3>
+    <h3>名前:{person.age}</h3>
+    <input type ="text" value={person.name} onChange={changeName}/>
+    <input type ="age" value={person.age} onChange={changeAge}/>
+    <div>
+      <button onClick = {reset}>リセット</button>
+    </div>
+  </>);
 };
 
 export default Example;
